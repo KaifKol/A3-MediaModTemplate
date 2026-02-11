4e755f885b452f7aea3852d36898369e09d9aaf8
KaifKol <komrad02kb2020@gmail.com>
2026-02-11 08:47:24 +0200

    fix path

--- addons/MediaModTemplate/soundlist.hpp
+++ addons/MediaModTemplate/soundlist.hpp
@@ -1,12 +1,12 @@
 class template
 {
 	name = "template"; // display name
-	sound[] = { "sound\template.ogg", 1, 1, 100 }; // file, volume, pitch, maxDistance
+	sound[] = { "MediaModTemplate\sound\template.ogg", 1, 1, 100 }; // file, volume, pitch, maxDistance
 	titles[]={}; // subtitles
 };
 class template1
 {
 	name = "template1";
-	sound[] = { "sound\template1.ogg", 1, 1, 100 };
+	sound[] = { "MediaModTemplate\sound\template1.ogg", 1, 1, 100 };
 	titles[]={};
 };
